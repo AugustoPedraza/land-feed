a266ac5d2886cb2f6a76193376617f5a979c11dd
Augusto Pedraza <augusto.pedraza08@gmail.com>
2018-05-04 18:08:11 -0300

    Added getting started Apollo React code sample

--- frontend/land_feed/src/App.js
+++ frontend/land_feed/src/App.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+// import gql from "graphql-tag";
+
+import ExchangeRates from './ExchangeRates';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+
+  const client = new ApolloClient({
+    uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+  });
+// client
+//   .query({
+//     query: gql`
+//       {
+//         rates(currency: "USD") {
+//           currency
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result));
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to My React</h1>
+          </header>
+          <ExchangeRates />
+        </div>
+      </ApolloProvider>
     );
   }
 }
